022296ba16223bde9698bde7a8a4793255685951
Robert Paul <Robert.F.Paul@gmail.com>
2018-01-19 00:51:17 -0600

    So close! Algorithm looks like it's working ok, need to tweak some more things though.

--- DiscussionGrouper.R
+++ DiscussionGrouper.R
@@ -33,7 +33,8 @@
 # Sum of math score categories is 0 or 1
 # If a minority is present in a group, at least 2 of the same minority are in the group
 # Sum of personality types is 0 or 1
-# For the generated sets of groups:
+
+# For the generated sets of groups included in the Hall of Fame:
 # Maximize uniqueness between grouping solutions
 
 ## Package imports
@@ -68,44 +69,104 @@ CatScores <-function(scores)
   return(score_cats)
 }
 
-# Split into groups
-SplitStudents <- function(df, arrangement)
+# Given the data frame and column, return a list of values within the column
+# and split by group
+SplitExtracted <- function(df, c)
 {
-  splitDf <- split(df, arrangement)
-  
-  # Sort within each data frame in the list by ID column
-  splitDf <- lapply(splitDf, function(df){df[order(df$ID),]})
-  
-  # Return result
-  return(splitDf)
+  result <- df[c]
+  result <- split(result, df$GroupID)
+  return(result)
 }
 
-# Evaluate "fitness" for number and sizes of groups
-# +0.1 to fitness per group with 3 or 4 members
-# If all groups meet criteria, fitness set to +1
+# Evaluate fitness for size and number of groups
+EvalGroupSize <- function(critVals) {
+  # Check if all the groups have 3 or 4 members and we have the right
+  # number of groups
+  sizes <- lapply(critVals, nrow)
+  sizesOK <- prod(sizes == 3 | sizes == 4)
+  if(sizesOK) {
+    # Number of groups and sizes ok, fitness +1
+    return(1)
+  } else { # Fitness penalty when groups improperly sized or wrong number of groups
+    return(0)
+  }
+}
 
 # Evaluate "fitness" for gender compositions
-# Sum the gender composition of each group
-# +0.1 to fitness per group with a sum of 0, 3, or 4
-# If all groups meet criteria, fitness set to +1
+EvalGender <- function(critVals)
+{
+  # Sum the gender composition of each group
+  sumVec <- as.numeric(lapply(critVals, sum))
+  # Get the size of each group
+  sizeVec <- as.numeric(lapply(critVals, nrow))
+  # +0.1 to fitness per group with a sum of 0 or sum equal to group size
+  # But first we need to redefine the values in the vector
+  sumVec[sumVec == 0 | sumVec == sizeVec] <- .1
+  sumVec[sumVec > .1] <- 0
+  fitness <- sum(sumVec)
+  # If all groups meet criteria, fitness set to +1
+  if (length(unique(sumVec)) == 1 & fitness != 0) fitness <- 1
+  # return fitness value
+  return(fitness)
+  
+  # # Trying something else...
+  # # Does every group have only one gender represented among its members?
+  # genderSplit <- prod((lapply(critVals, function(x) nrow(unique(x)))) == 1)
+  # if (genderSplit) {
+  #   # Homogeneous groups have +1 fitness
+  #   return(1)
+  # } else {
+  #   # Fitness penalty for mixed
+  #   return(0)
+  # }
+}
 
-# Evaluate "fitness" for math percentile distributions
-# Sum the score percentile category
-# +0.1 to fitness per group with a sum of 1
-# If all groups meet criteria, fitness set to +1
+# Evaluate "fitness" for math percentile distributions AND evaluate "fitness" 
+# for only including at most one "leader" personality
+EvalML <- function(critVals)
+{
+  # Sum the score or leader category and cast the list as a vector
+  critVals <- as.numeric(lapply(critVals, sum))
+  # +0.1 to fitness per group with a sum of 1 or 0
+  # But first we need to redefine the values in the vector
+  critVals[critVals <= 1] <- .1
+  critVals[critVals > .1] <- 0
+  fitness <- sum(critVals)
+  # If all groups meet criteria, fitness set to +1
+  if (fitness == .1 * length(critVals)) fitness = 1
+  # return fitness value
+  return(fitness)
+}
 
 # Evaluate "fitness" for diversity
+# Not yet implemented
 
 # length(unique(Ethnicity)) == 1
 # +0.1 to fitness per group meeting criteria
 # If all groups meet criteria, fitness set to +1
 
-# Evaluate "fitness" for only including at most one "leader" personality
-# Sum the leader category
-# +0.1 to fitness per group with a sum of 1 or 0
-# If all groups meet criteria, fitness set to +1
+# All the fitness functions wrapped up to return a vector of fitness values
+# **RFP: I know I shouldn't use all these globals in here but I didn't want to pass
+# a ton of parameters into this and potentially muck things up badly
+MetaFitness <- function(indiv)
+{
+  currGroup <- classData
+  currGroup$GroupID <- indiv
+  result <- c(EvalGroupSize(SplitExtracted(currGroup, "ID")),
+              EvalGender(SplitExtracted(currGroup, "Gender")),
+              EvalML(SplitExtracted(currGroup, "Score_Cat")),
+              EvalML(SplitExtracted(currGroup, "Leader"))
+              )
+  # Return the weighted results
+  return(result * weighting)
+}
 
-# Evaluate "fitness" for uniqueness versus previous arrangement solutions
+# Get all the unique values that max out the parameters
+# Not yet implemented
+UpdateHoF <- function(hof, fit, pop) {
+  
+  return(hof)
+}
 
 ## Main script run
 
@@ -120,19 +181,106 @@ nStudents <- nrow(classData)
 # If we have at least 15 students...
 if (nStudents >= 15)
 {
-  # Place them into 5 groups until everyone is assigned
+  # Place them into 5 groups
   nGroups <- 5
 } else # But if we have 14 or fewer students...
 {
-  # Place them into 4 groups until everyone is assigned
+  # Place them into 4 groups
   nGroups <- 4
 }
+# Initial group arrangement; this is shuffled to produce the initial population
 initArrang <- rep(1:nGroups, 5)[1:nStudents]
 
+# Categorize the scores into a new column named Score_Cat
 classData$Score_Cat <- CatScores(classData$Score)
-initGroup <- SplitStudents(classData, initArrang)
 
-# Run genetic algorithm
+initGroup <- classData
+initGroup$GroupID <- initArrang
+
+# Set up and run genetic algorithm
+MU = 50L # Number of individuals
+LAMBDA = 20L # Number of offspring
+MAX.ITER = 100L # Max number of generations
+# Relative weighting of each parameter
+weighting <-  c(1, # Correct number and sizes of groups
+                1, # Homogeneous gender groups
+                1, # At most 1 person outside the middle 80% of scores
+                1) # At most 1 leader
+ref.point <- weighting # Ideal fitness values
+
+# Toolbox initialization
+# MetaFitness gives our vector of fitnesses, 4 objectives, maximize objectives
+control <- initECRControl(MetaFitness, n.objectives = 4L, minimize = FALSE)
+# Initialize the mutation, survival, and reproduction operations
+control <- registerECROperator(control, "mutate", mutScramble)
+control <- registerECROperator(control, "recombine", recUnifCrossover)
+control <- registerECROperator(control, "selectForMating", selSimple)
+control <- registerECROperator(control, "selectForSurvival", selNondom)
+
+# Initialize population of random arrangements
+population <- initPopulation(mu = MU, gen.fun = gen, expr = sample(initArrang))
+# And get the initial fitness
+fitness <- evaluateFitness(control, population)
+
+# Initialize logger
+log <- initLogger(control,
+                  log.stats = list(fitness = list("mean", "HV" = list(
+                    fun = computeHV,
+                    pars = list(ref.point = ref.point)))),
+                  init.size = MAX.ITER)
+updateLogger(log, population = population, fitness = fitness, n.evals = MU)
+
+# Initialize Pareto archive
+parchive <- initParetoArchive(control)
+updateParetoArchive(parchive, population, fitness)
+
+# Initialize Hall of Fame as an empty list
+hof <- list()
+
+for (i in seq_len(MAX.ITER)) {
+  # Generate offspring by recombination and mutation
+  offspring <- recombinate(control = control, 
+                           inds = population,
+                           fitness = fitness, 
+                           lambda = LAMBDA,
+                           p.recomb = 0.8)
+  offspring <- mutate(control,
+                      offspring,
+                      p.mut = 0.3)
+  
+  # Calculate costs of new arrangment population
+  fitness.o <- evaluateFitness(control, offspring)
+  
+  # Apply selection
+  sel <- replaceMuPlusLambda(control = control,
+                             population = population,
+                             offspring = offspring,
+                             fitness = fitness,
+                             fitness.offspring = fitness.o)
+  population <- sel$population
+  fitness <- sel$fitness
+  
+  # Update log, Pareto archive, and Hall of Fame with new values
+  updateLogger(log, population = population, fitness = fitness, n.evals = MU)
+  updateParetoArchive(parchive, population, fitness)
+  hof <- UpdateHoF(hof, population, fitness)
+}
+
+# Previous attempt using a "black box" wrapper
+# gaRun <- ecr(fitness.fun = MetaFitness,
+#              n.objectives = 3,
+#              n.dim = 3,
+#              minimize = FALSE,
+#              mu = MU,
+#              lambda = LAMBDA,
+#              lower = lower,
+#              upper = upper,
+#              representation = "permutation",
+#              perm = initArrang,
+#              initial.solutions = population,
+#              parent.selector = selSimple,
+#              survival.selector = selNondom,
+#              terminators = list(stopOnIters(MAX.ITER)))
 
 # Extract and format Pareto-optimal solutions
 
@@ -145,9 +293,9 @@ initGroup <- SplitStudents(classData, initArrang)
 # D, V             2               1               4               1
 # F, C             2               4               1               5
 # ...
-write.csv(results,
-          file=file.choose(filters = c("Comma Delimited Files (.csv)","*.csv")), 
-          row.names = FALSE)
+# write.csv(results,
+#           file=file.choose(filters = c("Comma Delimited Files (.csv)","*.csv")), 
+#           row.names = FALSE)
 
 ## First attempt -- for reference
 # StudentList$Race=as.character(StudentList$Race)
